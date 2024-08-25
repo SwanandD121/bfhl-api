5f217f534b018c0e1012aee686d971d676c81403
Swanand Deshpande <swananddeshpande121@gmail.com>
2024-08-25 12:19:15 +0530

    third

--- index.js
+++ index.js
@@ -49,4 +49,10 @@ app.use((req, res) => {
 //     console.log(`Server is running on port ${PORT}`);
 //   });
 // }
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 module.exports = app;
